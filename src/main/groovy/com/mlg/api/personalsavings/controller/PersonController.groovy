12919d54b4157225d40b04d87c30cf4d818c525a
johnnygomes84 <johnny.gomes84@yahoo.com>
2023-11-15 14:23:11 +0000

    working on the api, transactions ok

--- src/main/groovy/com/mlg/api/personalsavings/controller/PersonController.groovy
+++ src/main/groovy/com/mlg/api/personalsavings/controller/PersonController.groovy
@@ -6,6 +6,7 @@ import com.mlg.api.personalsavings.service.PersonService
 import groovy.transform.TupleConstructor
 import org.springframework.data.domain.Page
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin("*")
 @TupleConstructor(includeFields = true, defaults = false)
 @RequestMapping("/api/person")
 class PersonController {
